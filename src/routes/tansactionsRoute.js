f31b129e694ec724a33d8fd5f4b72d2e78f1859c
prashant verma <prashantverma1601@gmail.com>
2025-10-04 00:47:52 +0530

    cron.js added

---
+++ src/routes/tansactionsRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { sql } from "../config/db.js";
+import {
+  createTransaction,
+  deleteTransaction,
+  getSummaryByUserId,
+  getTransactionByUserId,
+} from "../controllers/transactionController.js";
+const router = express.Router();
+router.get("/:userId", getTransactionByUserId);
+
+router.post("/", createTransaction);
+
+router.delete("/:id", deleteTransaction);
+
+router.get("/summary/:userId", getSummaryByUserId);
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import transactionsRoute from "./routes/tansactionsRoute.js";
+import cors from "cors";
+import job from "./config/cron.js";
+dotenv.config();
+
+const app = express();
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+app.use(cors());
+app.use(rateLimiter);
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK", message: "API is healthy" });
+});
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
